a70a95278d6586d691ced5ec7ad361f2d829bd97
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-14 04:56:26 +0530

    added productlist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList';
 
 function App() {
+  const productList = [
+    {
+      price : 14000,
+      name : "hp pavillion",
+      quantity : 2
+    },
+    {
+      price : 34000,
+      name : "lenovo thinkpad l380",
+      quantity : 1
+    }
+  ]
   return (
     <>
     <Navbar/>
+    <ProductList productList={productList}/>
     </>
   );
 }
