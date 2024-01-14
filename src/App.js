b1818b4bd49e1107192c3a90c3ec06cedd0d2abc
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-14 05:50:36 +0530

    added price badge and quantity buttons group

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ function App() {
     {
       price : 14000,
       name : "hp pavillion",
-      quantity : 2
+      quantity : 0
     },
     {
       price : 34000,
       name : "lenovo thinkpad l380",
-      quantity : 1
+      quantity : 0
     }
   ]
   return (
     <>
     <Navbar/>
+    <main className='container mt-5'>
     <ProductList productList={productList}/>
+    </main>
     </>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function product(props) {
   return (
     <>
-        <div className='row'>
-            <div className='col-6'>
-                <h2>{props.product.name}</h2>
-            </div>
+      <div className="row">
+        <div className="col-6">
+          <h2>
+            {props.product.name}
+            <span className="badge bg-secondary">{props.product.price}</span>
+          </h2>
         </div>
+        <div className="col-4">
+          <div
+            class="btn-group"
+            role="group"
+            aria-label="Basic mixed styles example"
+          >
+            <button type="button" class="btn btn-danger">
+              -
+            </button>
+            <button type="button" class="btn btn-warning">
+              {props.product.quantity}
+            </button>
+            <button type="button" class="btn btn-success">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
